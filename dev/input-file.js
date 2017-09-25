8366163a5e2ff27456d11240b695577baee07103
Yevhen Tiurin <yevhentiurin@gmail.com>
2017-09-25 15:13:59 +0300

    Add form

--- dev/input-file.js
+++ dev/input-file.js
@@ -1,12 +1,18 @@
-function fileChanged(input)
+function filesChanged(files)
 {
-  for (var i = 0; i < input.files.length; i++) {
-    downscale(input.files[i], 400, 400)
-
-    .then(function(dataURL) {
-      var destImg = document.createElement('img')
-      destImg.src = dataURL
-      document.body.appendChild(destImg)
+  for (var i = 0; i < files.length; i++) {
+    downscale(files[i], 400, 400).
+    then(function(dataURL) {
+      var destInput = document.createElement("input");
+      destInput.type = "hidden";
+      destInput.name = "image[]";
+      destInput.value = dataURL;
+      // Append image to form as hidden input
+      document.forms[0].appendChild(destInput);
+      // Preview image
+      var destImg = document.createElement("img");
+      destImg.src = dataURL;
+      document.body.appendChild(destImg);
     })
   }
 }

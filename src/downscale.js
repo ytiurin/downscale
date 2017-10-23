57370abe23486de953326f677cd82acb14756c0d
Eugene Tiurin <yevhentiurin@gmail.com>
2017-10-24 02:55:49 +0300

    Add adaptive resizing

--- src/downscale.js
+++ src/downscale.js
@@ -89,14 +89,23 @@ function remapDimensions(destWidth, destHeight, sourceX, sourceY, sourceWidth,
   var origSourceWidth  = sourceWidth
   var origSourceHeight = sourceHeight
 
-  var destRatio   = destWidth / destHeight
   var sourceRatio = sourceWidth / sourceHeight
 
+  if (destWidth === 0) {
+    destWidth = destHeight * sourceRatio >> 0
+  }
+
+  if (destHeight === 0) {
+    destHeight = destWidth / sourceRatio >> 0
+  }
+
+  var destRatio   = destWidth / destHeight
+
   if (destRatio > sourceRatio) {
-    sourceHeight = sourceWidth / destRatio
+    sourceHeight = sourceWidth / destRatio >> 0
   }
   else {
-    sourceWidth = sourceHeight * destRatio
+    sourceWidth = sourceHeight * destRatio >> 0
   }
 
   var sourceX = sourceX || (origSourceWidth  - sourceWidth)  / 2 >> 0

0d2130c3c26fe22251dd4f94145ba2f3dc01cac7
Eugene Tiurin <yevhentiurin@gmail.com>
2017-10-31 17:52:34 +0200

    Add imageSmoothingEnabled support

--- src/canvas.js
+++ src/canvas.js
@@ -7,6 +7,13 @@ function resizeWithCanvas(canvas, source, destWidth, destHeight, sourceX,
   canvas.height = destHeight
 
   var ctx = canvas.getContext("2d")
+
+  ctx.mozImageSmoothingEnabled    = true
+  ctx.imageSmoothingQuality       = "high"
+  ctx.webkitImageSmoothingEnabled = true
+  ctx.msImageSmoothingEnabled     = true
+  ctx.imageSmoothingEnabled       = true
+
   ctx.drawImage(source,
     sourceX, sourceY, sourceWidth, sourceHeight,
     0, 0, destWidth, destHeight)
